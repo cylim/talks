944a838d708486ac9cb6c1da5281bc9e0bbc3ef7
CY Lim <cylim226@gmail.com>
2018-11-13 16:31:44 +0800

    add shuffle form

--- 201811-js-meetup-intro-react-hooks/demo/07.js
+++ 201811-js-meetup-intro-react-hooks/demo/07.js
@@ -5,29 +5,45 @@ class Input extends React.PureComponent {
     return (
       <div className="form-group">
         <label for={title}>{title}</label>
-        <input className="form-control" id={title} {...props}/>
+        <input className="form-control" id={title} {...props} />
       </div>
     )
   }
 }
 
-const inputs = [
-  {key: 'email', type: 'email', title: 'email'},
-  {key: 'name', type: 'text', title: 'name', maxlength: 5 },
-  {key: 'age', type: 'number', title: 'age', required: true},
-]
+class Root extends React.Component {
+  state = {
+    inputs: [
+      {key: 'email', type: 'email', title: 'email'},
+      {key: 'name', type: 'text', title: 'name', maxlength: 5},
+      {key: 'age', type: 'number', title: 'age', required: true},
+    ],
+  }
 
-class Root extends React.PureComponent {
+  componentDidMount() {
+    this.internalTimer = setInterval(() => {
+      this.setState((prev) => ({
+        inputs: prev.inputs.sort(() => 0.5 - Math.random())
+      }))
+    }, 2000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.internalTimer)
+  }
+
   handleSubmit = event => {
     event.preventDefault()
 
-    const [email, name, age] = event.target
+    const { email, name, age } = event.target.elements
+
     console.log(email.value)
     console.log(name.value)
     console.log(age.value)
   }
 
   render() {
+    const {inputs} = this.state
     return (
       <form onSubmit={this.handleSubmit}>
         {inputs.map(input => (
