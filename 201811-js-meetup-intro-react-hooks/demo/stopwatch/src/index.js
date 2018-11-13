88f0b3fee9fd707da2ab43dad1841b4c1d176841
CY Lim <cylim226@gmail.com>
2018-11-13 17:36:47 +0800

    update react 16.7

--- 201811-js-meetup-intro-react-hooks/demo/stopwatch/src/index.js
+++ 201811-js-meetup-intro-react-hooks/demo/stopwatch/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import App from './Stopwatch';
+// import App from './StopwatchFinal';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
